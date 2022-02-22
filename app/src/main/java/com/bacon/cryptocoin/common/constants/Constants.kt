cf467fa12777117464f4a484b61f9ab7db15874c
Admin <baconnkl@gmail.com>
2022-02-22 14:36:38 +0300

    change BASE_URL

--- app/src/main/java/com/bacon/cryptocoin/common/constants/Constants.kt
+++ app/src/main/java/com/bacon/cryptocoin/common/constants/Constants.kt
@@ -1,6 +1,5 @@
 package com.bacon.cryptocoin.common.constants
 
 object Constants {
-    const val BASE_URL = "https://api.coincap.io/"
-    const val API_KEY = "ebeb3c92-c517-4c48-b60c-ec73820331bd"
+    const val BASE_URL = "https://api.coinstats.app/public/v1/"
 }
