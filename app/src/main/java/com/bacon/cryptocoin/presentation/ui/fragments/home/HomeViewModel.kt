92f21821b04fc5f1513b863f571c20c5e324a41b
Admin <baconnkl@gmail.com>
2022-02-22 14:38:01 +0300

    delete paging

--- app/src/main/java/com/bacon/cryptocoin/presentation/ui/fragments/home/HomeViewModel.kt
+++ app/src/main/java/com/bacon/cryptocoin/presentation/ui/fragments/home/HomeViewModel.kt
@@ -1,15 +1,26 @@
 package com.bacon.cryptocoin.presentation.ui.fragments.home
 
-import androidx.lifecycle.viewModelScope
-import androidx.paging.cachedIn
 import com.bacon.cryptocoin.common.base.BaseViewModel
-import com.bacon.cryptocoin.data.repositories.CoinsRepository
+import com.bacon.cryptocoin.domain.usecases.FetchCoinsUseCase
+import com.bacon.cryptocoin.presentation.models.CoinsItemUI
+import com.bacon.cryptocoin.presentation.models.toUI
+import com.bacon.cryptocoin.presentation.state.UIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
-    private val repository: CoinsRepository
+    private val useCase: FetchCoinsUseCase,
 ) : BaseViewModel() {
 
-    fun fetchCoins() =
-        repository.fetchCoins().cachedIn(viewModelScope)
+    private val _fetchCoinsState = MutableStateFlow<UIState<List<CoinsItemUI>>>(UIState.Loading())
+    val fetchCoinsState: StateFlow<UIState<List<CoinsItemUI>>> = _fetchCoinsState
+
+    init {
+        fetchCoins()
+    }
+    private fun fetchCoins() {
+        _fetchCoinsState.subscribeTo({ useCase() },
+            { it -> it.map { it.toUI() } })
+    }
 }
