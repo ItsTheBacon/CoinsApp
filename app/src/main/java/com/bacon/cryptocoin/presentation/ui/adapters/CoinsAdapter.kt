0511a72c892dba3ad587f6f611fdbd23d15248e8
Admin <baconnkl@gmail.com>
2022-02-22 14:34:20 +0300

    change model

--- app/src/main/java/com/bacon/cryptocoin/presentation/ui/adapters/CoinsAdapter.kt
+++ app/src/main/java/com/bacon/cryptocoin/presentation/ui/adapters/CoinsAdapter.kt
@@ -2,18 +2,19 @@ package com.bacon.cryptocoin.presentation.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bacon.cryptocoin.R
+import coil.load
 import com.bacon.cryptocoin.common.base.BaseDiffUtilItemCallback
-import com.bacon.cryptocoin.data.remote.dtos.CoinsDto
+import com.bacon.cryptocoin.common.extensions.setOnSingleClickListener
 import com.bacon.cryptocoin.databinding.ItemCoinsBinding
+import com.bacon.cryptocoin.presentation.models.CoinsItemUI
 import com.bacon.cryptocoin.presentation.ui.adapters.CoinsAdapter.ViewHolder
 
 
 class CoinsAdapter(
-    private val onItemClick: (id: String) -> Unit
-) : PagingDataAdapter<CoinsDto, ViewHolder>(BaseDiffUtilItemCallback()) {
+    private val onItemClick: (id: String) -> Unit,
+) : ListAdapter<CoinsItemUI, ViewHolder>(BaseDiffUtilItemCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -32,21 +33,21 @@ class CoinsAdapter(
     }
 
     inner class ViewHolder(
-        private val binding: ItemCoinsBinding
+        private val binding: ItemCoinsBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
 
         init {
-            itemView.setOnClickListener {
-                getItem(absoluteAdapterPosition).apply { onItemClick(this?.id.toString()) }
+            itemView.setOnSingleClickListener {
+                getItem(absoluteAdapterPosition)?.apply {
+                    onItemClick(this.id.toString())
+                }
             }
         }
 
-        fun onBind(coin: CoinsDto) = with(binding) {
-            rank.text = itemView.context.getString(R.string.rank_coins, coin.rank)
+        fun onBind(coin: CoinsItemUI) = with(binding) {
             name.text = coin.name
             symbol.text = coin.symbol
-
+            itemImage.load(coin.icon)
         }
-
     }
 }
