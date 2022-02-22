7f5ce2df1ec776ee803514e51ffdb711277b6e07
Admin <baconnkl@gmail.com>
2022-02-22 14:35:50 +0300

    change fetching detail

--- app/src/main/java/com/bacon/cryptocoin/presentation/ui/fragments/detail/CoinsDetailViewModel.kt
+++ app/src/main/java/com/bacon/cryptocoin/presentation/ui/fragments/detail/CoinsDetailViewModel.kt
@@ -1,24 +1,29 @@
 package com.bacon.cryptocoin.presentation.ui.fragments.detail
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.bacon.cryptocoin.common.base.BaseViewModel
-import com.bacon.cryptocoin.data.remote.dtos.CoinsDto
-import com.bacon.cryptocoin.data.repositories.CoinsDetailRepository
+import com.bacon.cryptocoin.domain.usecases.FetchCoinDetailUseCase
+import com.bacon.cryptocoin.presentation.models.CoinDetailUI
+import com.bacon.cryptocoin.presentation.models.toUI
 import com.bacon.cryptocoin.presentation.state.UIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class CoinsDetailViewModel @Inject constructor(
-    private val repository: CoinsDetailRepository
+    private val useCase: FetchCoinDetailUseCase,
 ) : BaseViewModel() {
 
-    private val _fetchCoinDetail = MutableLiveData<UIState<CoinsDto>>()
-    val fetchCoinsDetail: LiveData<UIState<CoinsDto>> = _fetchCoinDetail
+    private val _fetchCoinDetail = MutableStateFlow<UIState<List<CoinDetailUI>>>(UIState.Loading())
+    val fetchCoinsDetail: StateFlow<UIState<List<CoinDetailUI>>> = _fetchCoinDetail
 
     fun fetchCoinDetail(id: String) {
-        subscribeTo(_fetchCoinDetail) {
-            repository.fetchCoinDetail(id)
-        }
+        _fetchCoinDetail.subscribeTo({
+            useCase(id)
+        }, { it ->
+            it.map {
+                it.toUI()
+            }
+        })
     }
 
 }
