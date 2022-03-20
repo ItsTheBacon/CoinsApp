965aa76b31dae55cb152684b2b1b7bb05ad25451
Admin <baconnkl@gmail.com>
2022-03-20 17:12:02 +0300

    added injectoir and create subComponents

--- app/src/main/java/com/bacon/cryptocoin/App.kt
+++ app/src/main/java/com/bacon/cryptocoin/App.kt
@@ -1,17 +1,36 @@
 package com.bacon.cryptocoin
 
 import android.app.Application
-import com.bacon.cryptocoin.di.AppComponent
-import com.bacon.cryptocoin.di.DaggerAppComponent
+import com.bacon.cryptocoin.di.Injector
+import com.bacon.cryptocoin.di.components.AppComponent
+import com.bacon.cryptocoin.di.components.DaggerAppComponent
+import com.bacon.cryptocoin.di.modules.AppModule
+import com.bacon.cryptocoin.di.modules.NetworkModule
+import com.bacon.cryptocoin.di.modules.RepositoryModule
+import com.bacon.cryptocoin.di.subcomponents.CoinsSubComponent
+import com.bacon.cryptocoin.di.subcomponents.coinsdetail.CoinsDetailSubComponent
 
-class App : Application() {
-    lateinit var appComponent: AppComponent
+class App : Application(), Injector {
+    private lateinit var appComponent: AppComponent
     override fun onCreate() {
         super.onCreate()
         initializeDagger()
+
     }
 
     private fun initializeDagger() {
-        appComponent = DaggerAppComponent.create()
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(applicationContext))
+            .networkModule(NetworkModule())
+            .repositoryModule(RepositoryModule())
+            .build()
+    }
+
+    override fun createCoinsSubComponent(): CoinsSubComponent {
+        return appComponent.coinsSubComponent().create()
+    }
+
+    override fun createCoinsDetailSubComponent(): CoinsDetailSubComponent {
+        return appComponent.coinsDetailSubComponent().create()
     }
 }
