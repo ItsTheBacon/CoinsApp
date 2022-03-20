3b274773807929b6aec25da837b5962e1e604bb0
Admin <baconnkl@gmail.com>
2022-03-20 17:16:19 +0300

    create CoinsSubComponent added Factory

---
+++ app/src/main/java/com/bacon/cryptocoin/di/subcomponents/CoinsSubComponent.kt
@@ -0,0 +1,15 @@
+package com.bacon.cryptocoin.di.subcomponents
+
+import com.bacon.cryptocoin.presentation.ui.fragments.home.HomeFragment
+import dagger.Subcomponent
+
+@CryptoScope
+@Subcomponent(modules = [CoinsModule::class])
+interface CoinsSubComponent {
+    fun inject(fragment: HomeFragment)
+
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(): CoinsSubComponent
+    }
+}
